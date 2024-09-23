ebdbb17582370a542ade1a6f4d289e4f98826b98
ObalaSax <120820140+ObalaSax@users.noreply.github.com>
2024-09-23 21:30:31 +0300

    W4 D1

---
+++ react-concepts/src/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>Read</h1>
+      <nav>
+        <ul>
+          <li>Read Me</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
